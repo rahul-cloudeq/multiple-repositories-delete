e6fef6b3636830b736ab726d42a5868de97f34c8
rahul-cloudeq <110091668+rahul-cloudeq@users.noreply.github.com>
2022-09-10 00:38:17 +0530

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -3,7 +3,8 @@ delete="test1 test2 test3"
 user="rahul-cloudeq" 
 password_or_oauth_token="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" 
  
-for repo in $delete do 
+for repo in $delete 
+do 
     echo "Ixnay on the eporay! https://github.com/$user/$repo" 
     curl -v -u "$user:$password_or_oauth_token" -X DELETE \ 
        "https://api.github.com/$user/$repo" 
