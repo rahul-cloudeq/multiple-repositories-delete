1170920e8f2cba5ff1388d92c8a68621941a5420
rahul-cloudeq <110091668+rahul-cloudeq@users.noreply.github.com>
2022-09-10 00:36:17 +0530

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -1,11 +1,9 @@
-#!/bin/sh 
+#!/bin/sh  
+delete="test1 test2 test3" 
+user="rahul-cloudeq" 
+password_or_oauth_token="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" 
  
-# Delete a bunch of GitHub repos 
-nukem="test1 test2 test3" 
-user="${{ USER }}" 
-password_or_oauth_token="${{ TOKEN }}" 
- 
-for repo in $nukem do 
+for repo in $delete do 
     echo "Ixnay on the eporay! https://github.com/$user/$repo" 
     curl -v -u "$user:$password_or_oauth_token" -X DELETE \ 
        "https://api.github.com/$user/$repo" 
